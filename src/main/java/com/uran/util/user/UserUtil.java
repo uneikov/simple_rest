3f816d8413c9762cc4e78ae82acb866a94b516de
IOURI NEIKOV <neikov@gmail.com>
2017-04-29 17:48:57 +0300

    add production profile for test

--- src/main/java/com/uran/util/user/UserUtil.java
+++ src/main/java/com/uran/util/user/UserUtil.java
@@ -1,16 +1,26 @@
 package com.uran.util.user;
 
 
+import com.uran.domain.Account;
 import com.uran.domain.Role;
 import com.uran.domain.User;
 import com.uran.dto.UserDto;
 import com.uran.util.PasswordUtil;
+import com.uran.util.RandomUtil;
 
 public class UserUtil {
     
     public static User createNewFromTo(UserDto newUser) {
-        //??? Account ???
-        return new User(null, newUser.getName(), newUser.getEmail().toLowerCase(), newUser.getPassword(), null, Role.ROLE_USER);
+        User preUser = new User();
+        Account account = new Account(RandomUtil.getRandomCardNumber(), 0.0, preUser);
+        return new User(
+                null,
+                newUser.getName(),
+                newUser.getEmail().toLowerCase(),
+                newUser.getPassword(),
+                account,
+                Role.ROLE_USER
+        );
     }
     
     public static UserDto asTo(User user) {
